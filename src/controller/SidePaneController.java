e6ffb077a4cae53aef580bc0d3204ebbaa406a37
SanoP <sanodeemantha@gmail.com>
2020-08-04 22:54:28 +0530

    Set Method to SidePane

--- src/controller/SidePaneController.java
+++ src/controller/SidePaneController.java
@@ -1,7 +1,42 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class SidePaneController {
     public AnchorPane pane;
+    public JFXButton btnMangeCustomers;
+    public JFXButton btnHandleCarPark;
+    public JFXButton btnMangePackages;
+    public JFXButton btnManageCarCells;
+    public JFXButton btnRegisterNewUser;
+    public JFXButton btnSettings;
+    public JFXButton btnLogout;
+    public JFXButton btnHandlePackages;
+
+    public void btnMangeCustomersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnHandleCarParkOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnMangePackageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageCarCellsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRegisterNewUserOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnHandlePackagesOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingsOnAction(ActionEvent actionEvent) {
+
+    }
 }
